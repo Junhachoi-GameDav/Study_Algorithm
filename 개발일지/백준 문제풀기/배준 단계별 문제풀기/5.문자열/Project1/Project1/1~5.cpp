d2e16f057f557b38f159cd7594f8d2f34643f713
junhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-07-18 00:18:43 +0900

    7-18

---
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/5.문자열/Project1/Project1/1~5.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	if(s.length() <= 100){ cout << s.length(); }
+}
+*/
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	string words[11];
+	for (int i = 1; i <= t; i++)
+	{
+		cin >> words[i];
+		cout << words[i].front() << words[i].back() << endl;
+	}
+}
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	char c;
+	cin >> c;
+	cout << int(c);
+}
