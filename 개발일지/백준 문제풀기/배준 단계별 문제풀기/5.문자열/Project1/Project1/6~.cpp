fed35ae572f424efaf80e862ff30af50053f4be2
junhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-07-19 00:02:33 +0900

    7-19

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/5.문자열/Project1/Project1/6~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/5.문자열/Project1/Project1/6~.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <string>
 using namespace std;
@@ -20,4 +21,11 @@ int main() {
 	{
 		cout << num[i] << " ";
 	}
+}
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
 }
