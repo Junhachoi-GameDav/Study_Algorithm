0c6c920d14f1dd4b734d4d12a60c33215b8c3103
junhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-07-18 21:12:16 +0900

    7-18

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/5.문자열/Project1/Project1/6~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/5.문자열/Project1/Project1/6~.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-
+	int n=0, num[100];
+	string s;
+	cin >> s;
+	
+	for (int i = 0; i < 26; i++)
+	{
+		num[i] = -1;
+	}
+	for (int j = 0; j < s.length(); j++)
+	{
+		if (num[int(s[j]) - 97] == -1) { num[int(s[j]) - 97] = n; }
+		n++;
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		cout << num[i] << " ";
+	}
 }
