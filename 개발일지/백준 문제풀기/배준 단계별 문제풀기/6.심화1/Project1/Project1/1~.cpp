294006544dbc05063f27ca13d11c7870f94b3e2e
junhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-07-21 23:31:57 +0900

    7-21

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/6.심화1/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/6.심화1/Project1/Project1/1~.cpp
@@ -11,3 +11,20 @@ int main() {
 	cout << "      |";
 }
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[6] = { 1,1,2,2,2,8 };
+	int b[6] = { 0 };
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> b[i];
+		if (a[i] < b[i] || a[i] > b[i]) { a[i] -= b[i]; }
+		else { a[i] = 0; }
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
