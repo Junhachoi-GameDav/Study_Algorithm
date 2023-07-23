cbd400bf958d3e067b96af107e7be5788f5fa0f3
junhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-07-23 13:13:33 +0900

    7-23

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/6.심화1/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/6.심화1/Project1/Project1/1~.cpp
@@ -71,6 +71,7 @@ int main() {
 	}
 }
 */
+/*
 #include <iostream>
 #include <string>
 using namespace std;
@@ -92,3 +93,166 @@ int main() {
 	}
 	cout << n;
 }
+*/
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	int counted[26]{ 0, };
+	int a;
+
+	for (int x = 0; x < s.length(); x++)
+	{
+		if ((int)s[x] >= 65 && (int)s[x] <= 90) {
+			a = (int)s[x] - 65;
+		}
+		else if ((int)s[x] >= 97 && (int)s[x] <= 122) {
+			a = (int)s[x] - 97;
+		}
+		counted[a] = counted[a] + 1;
+	}
+	
+	int max = counted[0];
+	int k = 0;
+
+	for (int i = 1; i <= 25; i++)
+	{
+		if (max < counted[i]) { max = counted[i]; k = i; }
+	}
+
+	for (int i = 1; i <= 25; i++)
+	{
+		if (i == k) { i = i + 1; }
+		else
+		{
+			if (max == counted[i])
+			{
+				cout << "?";
+				return 0;
+			}
+		}
+	}
+	cout << (char)(k + 65);
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+	int count = 0 , a=0;
+	int c[26] = { 0 };
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if ((int)str[i] >= 65 && (int)str[i] <= 90) {
+			a = (int)str[i] - 65;
+		}
+		else if ((int)str[i] >= 97 && (int)str[i] <= 122) {
+			a = (int)str[i] - 97;
+		}
+		c[a] = c[a] + 1;
+	}
+
+	int max = 0, max_index = 0;
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (max < (int)c[i])
+		{
+			max = (int)c[i];
+			max_index = i;
+		}
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (max == (int)c[i]) { count++; }
+	}
+
+	if (count > 1)
+	{ 
+		cout << "?";
+	}
+	else
+	{ 
+		cout << (char)(max_index + 65);
+	}
+	return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int a[26], cnt = 0;
+string s;
+
+int main() {
+	cin >> s;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] < 97) { a[s[i] - 65]++; } //대문자
+		else { a[s[i] - 97]++; } //소문자
+	}
+
+	int max = 0, max_index = 0;
+
+	for (int i = 0; i < 26; i++) //최대 값
+	{
+		if (max < a[i])
+		{
+			max = a[i];
+			max_index = i;
+		}
+	}
+
+	for (int i = 0; i < 26; i++) //여러개 확인
+	{
+		if (max == a[i]) { cnt++; }
+	}
+
+	if (cnt > 1) { cout << "?"; }
+	else { cout << (char)(max_index + 65); }
+}
+/*
+#include <iostream>
+using namespace std;
+
+int alpha[26], cnt = 0;
+string input;
+
+int main() {
+	cin >> input;
+
+	//빈도수 체킹
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] < 97) alpha[input[i] - 65]++; //대문자
+		else alpha[input[i] - 97]++; //소문자
+	}
+
+	int max = 0, max_indx = 0;
+
+	for (int i = 0; i < 26; i++) {
+		if (max < alpha[i]) {
+			max = alpha[i];
+			max_indx = i;
+		}
+	}
+	//가장 많이 사용된 알파벳이 여러개인가?
+	for (int i = 0; i < 26; i++) {
+		if (max == alpha[i]) cnt++;
+	}
+	//여러개라면 ? 출력, 아니라면 대문자로 출력
+	if (cnt > 1) cout << "?";
+	else cout << (char)(max_indx + 65);
+}
+*/
