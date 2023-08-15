108fe41a5e258c66f82706aa5a40782dd1f61ce0
junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2023-08-16 01:00:34 +0900

    8-16

---
+++ 개발일지/c,c++,c# 기초/c++/test_for_tutorial/test_for_tutorial/test_for_tutorial.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void Test() {
+    int a = 100;
+
+    cout << a << endl;
+}
+
+
+int main()
+{
+    int hp = 10;
+    Test();
+
+    int mp = 200;
+    
+}
