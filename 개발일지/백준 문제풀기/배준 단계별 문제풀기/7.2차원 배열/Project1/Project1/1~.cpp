65e6e398f5ec0ad99f05ba1ad18e23b495c89480
junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2023-08-17 19:04:56 +0900

    8-17

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/7.2차원 배열/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/7.2차원 배열/Project1/Project1/1~.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 int main() {
-	int n, m;
-
+	int n, m, count =0;
+	
 	cin >> n >> m;
 
-	
+	int* A = new int[n * m];
+	int* B = new int[n * m];
+	for (int i = 0; i < n * m; i++)
+	{
+		cin >> A[i];
+	}
+	for (int i = 0; i < n * m; i++)
+	{
+		cin >> B[i];
+	}
 
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << A[count] + B[count] << " ";
+			count++;
+		}
+		cout << endl;
+	}
 }
