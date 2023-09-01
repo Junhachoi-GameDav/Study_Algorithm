2c7a85bbab904a2a83ad30b2f5daf30729e8afa7
junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2023-09-01 22:32:50 +0900

    9-1

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/7.2차원 배열/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/7.2차원 배열/Project1/Project1/1~.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -26,4 +27,32 @@ int main() {
 		}
 		cout << endl;
 	}
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num[9][9] = { 0 };
+	int max = 0;
+	int num_x = 0, num_y = 0;
+
+	for (int x = 0; x < 9; x++)
+	{
+		for (int y = 0; y < 9; y++)
+		{
+			cin >> num[x][y];
+
+			if (max < num[x][y])
+			{
+				max = num[x][y];
+				num_x = x;
+				num_y = y;
+			}
+		}
+	}
+
+	cout << max << endl;
+	cout << num_x+1 << " " << num_y+1;
 }
