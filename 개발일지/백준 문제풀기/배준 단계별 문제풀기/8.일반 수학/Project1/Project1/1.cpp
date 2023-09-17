4e648e17acc11c9bd4275fffdc990984bfbadcc9
junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2023-09-17 16:41:46 +0900

    9-17

---
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/8.일반 수학/Project1/Project1/1.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int b=0, n=0, total=0;
+	string alphabet;
+
+	cin >> alphabet >> b;
+	n = alphabet.length();
+
+	for (int i = 0; i < n; i++)
+	{
+		if (alphabet[i] >= '0' && alphabet[i] <= '9') {
+			total = total * b + (alphabet[i] - 48);
+		}
+		else {
+			total = total * b + (alphabet[i] - 55);
+		}
+	}
+
+	cout << total;
+	return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
