a5ace7152000f3a691a53c15e2c181b6f63ce6f6
junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2023-09-24 21:11:09 +0900

    9-24

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/8.일반 수학/Project1/Project1/1.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/8.일반 수학/Project1/Project1/1.cpp
@@ -91,6 +91,7 @@ int main() {
 	}
 	return 0;
 }*/
+/*
 #include <iostream>
 using namespace std;
 
@@ -105,5 +106,23 @@ int main() {
 		defalt += temp;
 	}
 	cout << defalt * defalt;
+	return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int i = 0;
+	for (int sum = 2; sum <= n ; i++)
+	{
+		sum += 6 * i;
+	}
+	if (n == 1) { i = 1; }
+	cout << i;
+
 	return 0;
 }
