78da77d64e5cb637ee72d1e79d9466021d64374e
janhachoi <wnsgk7654@gmail.com>
2023-11-09 18:24:45 +0900

    11-9

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/9. 약수와 배수/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/9. 약수와 배수/Project1/Project1/1~.cpp
@@ -1,8 +1,55 @@
+//#1
+/*
 #include <iostream>
 using namespace std;
 
 int main() {
 
+	int num1=0, num2=0;
+
+	while (true)
+	{
+		cin >> num1 >> num2;
+		if (num1 == num2) { break; }
+
+		if (num2 % num1 == 0)
+		{
+			cout << "factor" << endl;
+		}
+		else if (num1 % num2 == 0)
+		{
+			cout << "multiple" << endl;
+		}
+		else
+		{
+			cout << "neither" << endl;
+		}
+	}
+	
+	return 0;
+}
+*/
+//#2
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int N = 0, K = 0;
+	int count = 0;
+	cin >> N >> K;
+	
+	for (int i = N; i >= 1; i--)
+	{
+
+		if (N % i == 0 && count < K)
+		{
+			cout << N / i;
+			count++;
+		}
+	}
+
 
 	return 0;
 }
+
