c73eeb88ad1299060f97e2482d1468697d4fa7d8
janhachoi <wnsgk7654@gmail.com>
2023-12-09 16:38:57 +0900

    2023-12-9

--- 개발일지/c,c++,c# 기초/c++/14. DataStructure/Project1/Maze/List.cpp
+++ 개발일지/c,c++,c# 기초/c++/14. DataStructure/Project1/Maze/List.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 #include "List.h"
+
+using namespace std;
+
+
+int main() {
+	List li;
+
+	li.AddAtTail(1);
+	Node* node = li.AddAtTail(2); //기억해놓았다가
+	li.AddAtTail(3);
+
+	li.Print();
+
+	//하나하나 찾아야되서 느리다.
+	//li.GetNode(2);
+
+	li.Remove(node); //지정해줘야 빠르게 삽입 삭제
+
+	li.Print();
+}
--- 개발일지/c,c++,c# 기초/c++/14. DataStructure/Project1/Maze/List.h
+++ 개발일지/c,c++,c# 기초/c++/14. DataStructure/Project1/Maze/List.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 class Node
 {
@@ -19,32 +21,108 @@ class List
 public:
 	List()
 	{
-
+		_head = new Node(0); //더미
+		_tail = new Node(0); //더미
+		_head->next = _tail;
+		_tail->prev = _head;
 	}
 	~List()
 	{
+		Node* node = _head;
+		while (node)
+		{
+			Node* deleteNode = node;
+			node = node->next; //한깐씩 이동하는 효과
+			delete deleteNode;
+		}
+	}
 
+	//하나하나 다찾아야 하기때문에 엄청 느리고 비효율
+	Node* GetNode(int index) //몇번째 인지 찾기
+	{
+		Node* node = _head->next;
+		if (node == _tail) { return nullptr; }
+
+		for (int i = 0; i < index; i++)
+		{
+			if (node == _tail->prev) { return nullptr; }
+
+			node = node->next;
+		}
+	}
+
+	void Print()
+	{
+		Node* node = _head->next;
+		while (node != _tail)
+		{
+			cout << node->data << " ";
+			node = node->next;
+		}
+		cout << endl;
 	}
 
 	//		[node]<->[next_node][2][3]
 	//[head]						[tail]
+	//이방식은 좀 비효율적이니 더미를 이용한 방법을 쓴다.
+	//		[dummy]<->[dummy]
+	//[head]				[tail]
 	Node* AddAtatHead(int data)
 	{
+		//		[dummy]<->[node]<->[next_node]<->[][]...[dummy]
+		//		[head]									[tail]
 		Node* node = new Node(data);
+		Node* next_node = _head->next;
 
-		if (_head == nullptr)
-		{
-			_head = node;
-			_tail = node;
-		}
-		else
-		{
-			//순차적으로 하면된다. 그림을 먼저 그려놓고
-			Node* next_node = _head;
-			node->next = next_node;
-			next_node->prev = node;
-			_head = node;
-		}
+		node->next = next_node;
+		next_node->prev = node;
+		_head->next = node;
+		node->prev = _head;
+
+		return node;
+	}
+
+	Node* AddAtTail(int data)
+	{
+		//		[dummy][][][]...[prev_node]<->[node]<->[dummy]
+		//		[head]									[tail]
+		Node* node = new Node(data);
+		Node* prev_node = _tail->prev;
+
+		prev_node->next = node;
+		node->prev = prev_node;
+		node->next = _tail;
+		_tail->prev = node;
+
+		return node;
+	}
+
+
+	//사실상 이거 배우려고 한거임. 삽입 삭제
+
+	void Insert(Node* pos_node, int data)
+	{
+		Node* node = new Node(data);
+		Node* prev_node = pos_node->prev;
+
+		prev_node->next = node;
+		node->prev = prev_node;
+		node->next = pos_node;
+		pos_node->prev = node;
+	}
+	//						[node]
+	//	[dummy]<->[prev_node]<->[next_node]<->[dummy]
+	//	[head]									[tail]
+	Node* Remove(Node* node)
+	{
+		Node* prev_node = node->prev;
+		Node* next_node = node->next;
+		prev_node->next = next_node;
+		next_node->prev = prev_node;
+
+		delete node;
+
+		return next_node;
 	}
 
 private:
