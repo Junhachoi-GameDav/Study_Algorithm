4e95c2291e714adbd9777af4b0223844b5eb51db
janhachoi <wnsgk7654@gmail.com>
2024-01-06 21:26:38 +0900

    2024-1-6

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/9. 약수와 배수/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/9. 약수와 배수/Project1/Project1/1~.cpp
@@ -36,20 +36,19 @@ using namespace std;
 int main() {
 
 	int N = 0, K = 0;
-	int count = 0;
+	int temp = 0, count =0;
 	cin >> N >> K;
 	
 	for (int i = N; i >= 1; i--)
 	{
-
 		if (N % i == 0 && count < K)
 		{
-			cout << N / i;
+			temp = N / i;
 			count++;
 		}
 	}
-
-
+	if (count < K) { temp = 0; }
+	cout << temp;
 	return 0;
 }
 
