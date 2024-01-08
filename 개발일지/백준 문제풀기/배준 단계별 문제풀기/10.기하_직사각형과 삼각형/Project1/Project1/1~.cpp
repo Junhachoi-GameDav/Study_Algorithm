0657946b05c9a7cd8ea47a388ab6c59c4f3eed72
janhachoi <wnsgk7654@gmail.com>
2024-01-08 20:27:30 +0900

    2024-1-8

---
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/10.기하_직사각형과 삼각형/Project1/Project1/1~.cpp
@@ -0,0 +1,16 @@
+//#1
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 0, b = 0;
+	cin >> a >> b;
+
+	if ((a >= 1 && a <= 100) || (b >=1 && b <= 100))
+	{
+		cout << b * a;
+	}
+	return 0;
+}
+*/
