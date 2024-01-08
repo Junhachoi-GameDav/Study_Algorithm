89361c575508fefb9f22d32e6f05a9cd32873087
janhachoi <wnsgk7654@gmail.com>
2024-01-08 22:03:50 +0900

    2024-1-8

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/10.기하_직사각형과 삼각형/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/10.기하_직사각형과 삼각형/Project1/Project1/1~.cpp
@@ -14,3 +14,19 @@ int main() {
 	return 0;
 }
 */
+//#2
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 0, y = 0, w = 0, h = 0;
+	cin >> x >> y >> w >> h;
+
+	int min = x;
+	if (y < min) { min = y; }
+	if (w - x < min) { min = w - x; }
+	if (h - y < min) { min = h - y; }
+
+	cout << min;
+	return 0;
+}
