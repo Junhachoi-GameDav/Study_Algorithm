8b981bc69a5bec45e30ec705658f83434b005c01
janhachoi <wnsgk7654@gmail.com>
2024-01-09 15:42:30 +0900

    2024-1-9

--- 개발일지/백준 문제풀기/배준 단계별 문제풀기/10.기하_직사각형과 삼각형/Project1/Project1/1~.cpp
+++ 개발일지/백준 문제풀기/배준 단계별 문제풀기/10.기하_직사각형과 삼각형/Project1/Project1/1~.cpp
@@ -15,6 +15,7 @@ int main() {
 }
 */
 //#2
+/*
 #include <iostream>
 using namespace std;
 
@@ -29,4 +30,31 @@ int main() {
 
 	cout << min;
 	return 0;
+}
+*/
+//#3
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x[4] = { 0 };
+	int y[4] = { 0 };
+	int count_x = 0;
+	int count_y = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> x[i] >> y[i];
+	}
+	for (int i = 1; i < 3; i++)
+	{
+		if (x[0] != x[i]) { count_x++; }
+		if (count_x == 1 && x[0] != x[i]) { x[3] = x[i]; }
+		else if (count_x == 2) { x[3] = x[0]; }
+
+		if (y[0] != y[i]) { count_y++; }
+		if (count_y == 1 && y[0] != y[i]) { y[3] = y[i]; }
+		else if (count_y == 2) { y[3] = y[0]; }
+	}
+	cout << x[3] << " " << y[3];
 }
