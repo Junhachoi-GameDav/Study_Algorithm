eac4c9a4467d583e24ecf5778cdc741c4b253979
junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2024-01-11 17:00:17 +0900

    2024-1-11

--- 개발일지/백준 문제풀기/자유형/Project1/Project1/소스.cpp
+++ 개발일지/백준 문제풀기/자유형/Project1/Project1/소스.cpp
@@ -2,6 +2,40 @@
 using namespace std;
 
 int main() {
+	int n, count = 0;
+	cin >> n;
+	string* s = new string[n];
+	int arr[26] = { 0 };
+	bool one_team = false;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s[i];
+	}
 	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i+1; j < n; j++)
+		{
+			if (s[i][0] == s[j][0]) { count++; }
+			if (count == 4)
+			{ 
+				one_team = true;
+				arr[s[i][0] - 97]++;
+				break;
+			}
+		}
+		count = 0;
+	}
+
+	if (!one_team) { cout << "PREDAJA"; }
+	else
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			char data = i + 97;
+			if (arr[i]) { cout << data; }
+		}
+	}
 	return 0;
 }
