fbe28fed7a9bf481a046f0d9f07a4f1e31a8d12c
junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2024-01-16 17:59:39 +0900

    2024-1-16

--- 개발일지/c,c++,c# 기초/c++/15. Algorithm/Project1/Project1/sort.cpp
+++ 개발일지/c,c++,c# 기초/c++/15. Algorithm/Project1/Project1/sort.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 #include <vector>
 #include <list>
+#include <queue>
 #include <algorithm>
+
+
+// 결론적으로 정렬은 O(NlogN)이라고 말할 수 있고 map이 있는데도 굳이 서칭할때 쓰는이유는 인벤토리 기능들 떄문이다.
+// 아이템 별로 정렬, 등급별로 정렬등 ui에서 많이쓴다고한다.
+// 버튼을 눌러서 정렬 시키는 기능에 들어간다고 보면 된다. O(NlogN)이라서 계속 쓰기엔 무리가 있다.
+
+
 
 // O(N^2)
 void BubbleSort(vector<int>& v)
@@ -41,12 +49,138 @@ void SelectionSort(vector<int>& v)
 	}
 }
 
+//O(NlogN)
 void HeapSort(vector<int>& v)
 {
+	priority_queue<int, vector<int>, greater<int>> pq;//우선순위 큐
+	
+	//O(NlogN)
+	for (int num : v)
+	{
+		pq.push(num);
+	}
+	v.clear();
 
+	//O(NlogN)
+	while (pq.empty() == false)
+	{
+		v.push_back(pq.top());	//O(1)
+		pq.pop();				//O(logN)
+	}
+}
+
+
+// 병합 정렬(중요~!) (크래프톤 코테에 나왔다고함) 이거 많이 쓴다고함
+// 분할 정복이란것을 이용함
+// - 분할 (Divide)		큰문제를 나누어서 간단하게 만듬
+// - 정복 (Conquer)		분할된 문제를 해결
+// - 결합 (Combine)		결과를 취합후 마무리
+
+void MergeResult(vector<int>& v, int left, int mid, int right)
+{
+	int left_idx = left;
+	int right_idx = mid + 1;
+
+	vector<int> temp;
+
+	while (left_idx <= mid && right_idx <= right) //집어넣어줌
+	{
+		if (v[left_idx] <= v[right_idx])
+		{
+			temp.push_back(v[left_idx]);
+			left_idx++;
+		}
+		else
+		{
+			temp.push_back(v[right_idx]);
+			right_idx++;
+		}
+	}
+
+	if (left_idx > mid) //왼쪽이 먼저 끝나면
+	{
+		while (right_idx <= mid)
+		{
+			temp.push_back(v[right_idx]);
+			right_idx++;
+		}
+	}
+	else
+	{
+		while (left_idx <= mid)
+		{
+			temp.push_back(v[left_idx]);
+			left_idx++;
+		}
+	}
+	//임시로들고있던 모든 데이터 temp를 덮어씌움
+	for (int i = 0; i < temp.size(); i++)
+	{
+		v[left + i] = temp[i];
+	}
+}
+
+//O(NlogN)
+void MergeSort(vector<int>& v, int left, int right)
+{
+	if (left >= right) return;
+
+	int mid = (left + right) / 2;
+
+	MergeSort(v, left, mid); //재귀로 다시 나눔
+	MergeSort(v, mid + 1, right);
+
+	MergeResult(v, left, mid, right);
+}
+
+
+// 퀵 정렬
+// 강의봐~
+
+int Partition(vector<int>& v, int left, int right)
+{
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
+
+	while (low <= high)
+	{
+		while (low <= right&& pivot>= v[low])
+		{
+			low++;
+		}
+
+		while (high >= left +1 && pivot <= v[high])
+		{
+			high--;
+		}
+
+		if (low < high) {
+			swap(v[low], v[high]);
+		}
+	}
+
+	swap(v[left], v[high]);
+	return high;
+}
+
+// O(NlogN) 최악일땐=pivot값이 제일 작거나 할때 O(N^2) 되기도한다.
+void QuickSort(vector<int>& v, int left, int right)
+{
+	if (left > right) { return; }
+
+	int pivot = Partition(v, left, right);
+
+	QuickSort(v, left, pivot - 1);
+	QuickSort(v, pivot + 1, right);
 }
 
 int main() {
 	vector<int> v{1, 5, 4, 2, 3};
 
+	//BubbleSort(v);
+	//SelectionSort(v);
+	//HeapSort(v);
+	//MergeSort(v, 0, v.size()-1);
+	QuickSort(v, 0, v.size() - 1);
 }
---
+++ 개발일지/c,c++,c# 기초/c++/15. Algorithm/Project1/Project1/string.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <list>
+#include <queue>
+#include <algorithm>
+
+// 로그인할때 문자들을 비교한다던가 생각보다 쓸일이 많다.
+
+
+int main() {
+
+	string str = "wwww";
+	string str2 = "wwww";
+
+
+	//비교
+	if (str == str2){}
+	str.compare(str2); // 1 or 0
+
+	//복사
+	string str3;
+	str3 = str;
+
+	//추가
+	// 계정을 지우면 진짜로 지우는게 아니라 케릭터 이름에
+	// wwww_DELETED 이런식으로 플레그를 만들어놓는다. 복원하기 위해서
+	str.append("123");
+	str += "123";
+
+	//찾기
+	auto c = str.find("ww"); //몇번째인지 인덱스를 뱉어줌
+	if (c == string::npos) // 끝까지 못찾았다면(없다면) vector에 !=v.end() 같은 느낌임
+	{
+		cout << "no found";
+	}
+
+	//교체
+	// 근데 자주쓸일이 없어서 까먹으면 그냥 구글 검색하고 만든다고한다. 
+	string chat_str = "SHIT~~!!";
+	string find_str = "SHIT";
+	string replace_str = "****";
+
+	chat_str.replace(chat_str.find(find_str), find_str.length(), replace_str);
+
+
+	// 멀티바이트와 유니코드
+	// 영어로만 서비스하면 몰라도 한글이 깨지거나 할수있다. 
+}
