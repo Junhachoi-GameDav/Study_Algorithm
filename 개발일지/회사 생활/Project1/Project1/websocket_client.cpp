0f71649d3a02567deb04acbd6f3d3aaffc321328
Junhachoi <87477736+Junhachoi-GameDav@users.noreply.github.com>
2024-03-20 18:10:06 +0900

    2024-03-20
    코드 리펙토링 변수, 함수 명 파스칼 케이스, 스네이크 케이스로 통일

--- 개발일지/회사 생활/Project1/Project1/websocket_client.cpp
+++ 개발일지/회사 생활/Project1/Project1/websocket_client.cpp
@@ -1,91 +1,107 @@
-#include "websocket_client.h"
-
-#include <utility>
-#include <regex>
+// std
+#include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
 #include <thread>
 
-#include <filesystem>
+#include "Websocket_client.h"
 
 using websocketpp::lib::bind;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 
-//초기화
-websocket_client::websocket_client(const std::string& uri) : uri(uri) {
-    c.init_asio();
-    c.clear_access_channels(websocketpp::log::alevel::all); //로그 다 지움
-    c.set_open_handler(bind(&websocket_client::on_open, this, _1));
-    c.set_message_handler(bind(&websocket_client::on_message, this, _1, _2));//Received message 로그에 띄움
-}
-
-websocket_client::~websocket_client()
+/** Websocket_client
+* @brief		- 초기화 생성자
+* @param	- uri 서버주소
+*/
+Websocket_client::Websocket_client(const std::string& uri) : uri(uri)
 {
-    if (con && con->get_state() == websocketpp::session::state::closed)
-        return;
-
-    websocketpp::lib::error_code ec;
-    con->close(websocketpp::close::status::normal, "", ec);
-
-    while (con->get_state() == websocketpp::session::state::closed)
-        std::this_thread::yield();
+	c.init_asio();
+	c.clear_access_channels(websocketpp::log::alevel::all); //로그 다 지움
+	c.set_open_handler(bind(&Websocket_client::on_open, this, _1));
+	c.set_message_handler(bind(&Websocket_client::on_message, this, _1, _2));//Received message 로그에 띄움
 }
 
-void websocket_client::on_open(websocketpp::connection_hdl hdl) {
-    
-    //사진파일 가져오기
-    const std::filesystem::path cur_path = std::filesystem::current_path(); //현재 디렉토리 가져옴
-    const std::filesystem::path img_path = cur_path / "image" / "IMG_03_0000000003_L.jpg";
-    
-    std::ifstream file(img_path, std::ios::binary);
-    if (!file.is_open())
-        return;
+Websocket_client::~Websocket_client()
+{
+	if (con && con->get_state() == websocketpp::session::state::closed)
+		return;
 
-    const std::string buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    c.send(hdl, buffer, websocketpp::frame::opcode::binary);
+	websocketpp::lib::error_code ec;
+	con->close(websocketpp::close::status::normal, "", ec);
+
+	while (con->get_state() == websocketpp::session::state::closed)
+		std::this_thread::yield();
 }
 
-void websocket_client::on_message(websocketpp::connection_hdl hdl, client::message_ptr msg) {
-    if (msg->get_opcode() != websocketpp::frame::opcode::text)
-        return;
+/** on_open
+* @brief		- 사진불러와서 바이너리로 서버에 보내기
+* @param	- hdl 웹소켓
+*/
+void Websocket_client::on_open(websocketpp::connection_hdl hdl)
+{
+	const std::filesystem::path cur_path = std::filesystem::current_path(); //현재 디렉토리 가져옴
+	const std::filesystem::path img_path = cur_path / "image" / "IMG_03_0000000003_L.jpg";
 
-    std::cout << "Received message: " << msg->get_payload() << '\n';
-    const std::string _polygon = msg->get_payload();
+	std::ifstream file(img_path, std::ios::binary);
+	if (!file.is_open())
+		return;
 
-    //정규포현식으로 polygon 점 분리
-    const std::regex reg(R"(\d+\s\d+)");
-    std::smatch matches;
-    auto searchStart(_polygon.cbegin());
-
-    while (std::regex_search(searchStart, _polygon.cend(), matches, reg))
-    {
-        std::istringstream iss(matches[0].str()); //matches 분리시겨줌
-        int x;
-        int y;
-        iss >> x >> y;
-        _poly_vec.push_back({ x, y }); //vector에 넣음
-
-        searchStart = matches.suffix().first;
-    }
-
-    c.stop();//클라이언트에서 더이상 이벤트 멈춤
+	const std::string buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	c.send(hdl, buffer, websocketpp::frame::opcode::binary);
 }
 
-void websocket_client::runAndGetPolygon() {
-   
-    websocketpp::lib::error_code ec;
-    con = c.get_connection(uri, ec);
+/** on_message
+* @brief		- 폴리곤 벡터에 저장
+* @param	- hdl 웹소켓, msg 폴리곤 메세지
+*/
+void Websocket_client::on_message(websocketpp::connection_hdl hdl, client::message_ptr msg)
+{
+	if (msg->get_opcode() != websocketpp::frame::opcode::text)
+		return;
 
-    if (ec)
-        return;
+	std::cout << "Received message: " << msg->get_payload() << '\n';
+	const std::string _polygon = msg->get_payload();
 
-    c.connect(con);
-    tg.run([this]()->void
-    {
-        c.run();
-    });
-    tg.wait();
+	//정규포현식으로 polygon 점 분리
+	const std::regex reg(R"(\d+\s\d+)");
+	std::smatch matches;
+	auto searchStart(_polygon.cbegin());
+
+	while (std::regex_search(searchStart, _polygon.cend(), matches, reg))
+	{
+		std::istringstream iss(matches[0].str()); //matches 분리시겨줌
+		int x;
+		int y;
+		iss >> x >> y;
+		_poly_vec.push_back({ x, y }); //vector에 넣음
+
+		searchStart = matches.suffix().first;
+	}
+
+	websocketpp::lib::error_code ec;
+	con->close(websocketpp::close::status::normal, "", ec);
+}
+
+/** get_polygon
+* @brief	- 서버에 접속후 폴리곤 받기
+* @brief	- 다른 쓰레드에서 실행됨
+*/
+void Websocket_client::get_polygon()
+{
+	websocketpp::lib::error_code ec;
+	con = c.get_connection(uri, ec);
+
+	if (ec)
+		return;
+
+	c.connect(con);
+	tg.run([this]()->void
+		{
+			c.run();
+		});
+	tg.wait();
 }
 
--- 개발일지/회사 생활/Project1/Project1/websocket_client.h
+++ 개발일지/회사 생활/Project1/Project1/websocket_client.h
@@ -1,32 +1,36 @@
 #pragma once
-#include <websocketpp/config/asio_no_tls_client.hpp>
-#include <websocketpp/client.hpp>
-
-#include <opencv2/core.hpp>
-
+// std
 #include <string>
 
+// websocketpp
+#include <websocketpp/client.hpp>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+
+// opencv
+#include <opencv2/core.hpp>
+
+// onetbb
 #include <oneapi/tbb/task_group.h>
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 
-class websocket_client {
+class Websocket_client
+{
 public:
-    websocket_client(const std::string& uri);
-    ~websocket_client();
-    void runAndGetPolygon();
+	Websocket_client(const std::string& uri);
+	~Websocket_client();
+	void get_polygon();
 
 private:
-    client c;
-    void on_open(websocketpp::connection_hdl hdl);
-    void on_message(websocketpp::connection_hdl hdl, client::message_ptr msg);
-    
+	client c;
+	void on_open(websocketpp::connection_hdl hdl);
+	void on_message(websocketpp::connection_hdl hdl, client::message_ptr msg);
+
 private:
-    const std::string uri;
-    //const std::thread thr;
-    client::connection_ptr con;
-    oneapi::tbb::task_group tg;
+	const std::string uri;
+	client::connection_ptr con;
+	oneapi::tbb::task_group tg;
 
 public:
-    std::vector<cv::Point> _poly_vec;
+	std::vector<cv::Point> _poly_vec;
 };
